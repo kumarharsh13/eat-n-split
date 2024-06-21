c36cd9f4eb9d780d1e306e85c2d818bfd97ffa26
kumarharsh13 <98450532+kumarharsh13@users.noreply.github.com>
2024-06-21 12:28:49 +0530

    Added Forms

--- src/App.js
+++ src/App.js
@@ -24,7 +24,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -65,7 +68,49 @@ function Friend({ friend }) {
         </p>
       )}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑 Friend Name</label>
+      <input type="text" />
+
+      <label>📸 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>💰 Money Value</label>
+      <input type="text" />
+
+      <label>🧍Your Expense</label>
+      <input type="text" />
+
+      <label>🧑‍🤝‍🧑 X's Expense</label>
+      <input type="text" disabled />
+
+      <label> 🤑Who is paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
