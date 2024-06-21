73ab3281068f2bb1027661cddb4442b259ee555c
kumarharsh13 <98450532+kumarharsh13@users.noreply.github.com>
2024-06-21 11:33:16 +0530

    Friend List

--- src/App.js
+++ src/App.js
@@ -19,8 +19,53 @@ const initialFriends = [
   },
 ];
 
-function App() {
-  return <div>"Eat-N-Split</div>;
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
+    </div>
+  );
 }
 
-export default App;
+function FriendsList() {
+  const friends = initialFriends;
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <li>
+          <Friend friend={friend} key={friend.id} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance === 0 && (
+        <p>
+          You and {friend.name} are even {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      <button className="button">Select</button>
+    </li>
+  );
+}
